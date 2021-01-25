95a3a5ec9f98da54a039e9ac84f1fb731f08594d
unknown <carlosmoraes_sis@hotmail.com>
2021-01-25 00:04:25 -0400

    criar comidinha

--- js/scrip.js
+++ js/scrip.js
@@ -6,8 +6,12 @@ snake[0] = {
     x: 8 * box,
     y: 8 * box
 }
-
 let direction = "right";
+
+let food = {
+   x : Math.floor(Math.random() * 15 + 1) * box,
+   y : Math.floor(Math.random() * 15 + 1) * box
+}
 
 
 
@@ -25,13 +29,18 @@ function criarCobrinha() {
 
 }
 
+function drawFood() {
+    context.fillStyle = "red";
+    context.fillRect(food.x, food.y, box, box);
+}
+
 document.addEventListener("keydown", update);
 
 function update (event) {
     if(event.keyCode == 37 && direction != "right") direction = "left";
     if(event.keyCode == 38 && direction != "down") direction = "up";
     if(event.keyCode == 39 && direction != "left") direction = "right";
-    if(event.keyCode == 40 && direction != "up") direction = "down";            
+    if(event.keyCode == 40 && direction != "up") direction = "down";                
 }
 
 function iniciarJogo() {
@@ -42,6 +51,7 @@ function iniciarJogo() {
 
     criarBG();
     criarCobrinha();
+    drawFood();
 
     let snakeX = snake[0].x;
     let snakeY = snake[0].y;
